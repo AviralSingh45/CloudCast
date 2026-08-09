479930e87ebe28a72f1f543eb7c0f95d934b4cf6
AviralSingh45 <aviralsingh129@gmail.com>
2026-08-09 09:52:12 +0530

    Add files via upload

---
+++ supabase/migrations/20260317045853_f878c1ee-5e46-4c08-aa17-9ee63f625a2b.sql
@@ -0,0 +1,19 @@
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO 'public'
+AS $function$
+BEGIN
+  INSERT INTO public.user_preferences (user_id)
+  VALUES (NEW.id);
+  RETURN NEW;
+END;
+$function$;
+
+-- Recreate trigger if not exists
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
+CREATE TRIGGER on_auth_user_created
+  AFTER INSERT ON auth.users
+  FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
